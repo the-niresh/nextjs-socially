8477327797d71fc0a40e921ed798a80e6dcbf87b
niresh <nireshine@gmail.com>
2025-02-05 00:36:55 +0530

    navbar completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import ModeToggle from "@/components/ModeToggle";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div>
+      <h1>Home</h1>
+    </div>
   );
 }
